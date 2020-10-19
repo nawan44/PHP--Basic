504d7543093ad37a515429a8dcbe4a3c3b9655b9
nawan44 <rachmat.d.gunawan.com>
2020-10-19 22:31:12 +0700

    comment

---
+++ comment.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+//single-line comment
+
+# single-line comment
+
+
+/*
+multiple-
+lines 
+comment 
+block
+*/
+
+
+// Comment inline code
+$x = 5 /* + 15 */ + 5;
+echo $x;
+?>
+
+</body>
+</html>
